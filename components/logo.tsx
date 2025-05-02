a8d81e80d591b68fa08d9a5457b6acfdb9f38698
stelly-dev <56655493+stelly-dev@users.noreply.github.com>
2025-05-01 20:57:15 -0600

    Refactor Logo component to accept customizable width and height props for improved flexibility

--- components/logo.tsx
+++ components/logo.tsx
@@ -1,8 +1,13 @@
 import Image from "next/image";
 import logo from '@/public/banner_logo_cropped_alpha.png'
 
-export default function Logo() {
+interface LogoProps {
+    width?: number;
+    height?: number;
+}
+
+export default function Logo({ width = 300, height = 100 }: LogoProps) {
     return (
-        <Image src={logo} alt="logo" width={300} height={100} />
+        <Image src={logo} alt="logo" width={width} height={height} />
     )
 }
