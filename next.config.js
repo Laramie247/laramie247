e0a116bf9100018e34070cbc5f383277459daa89
stelly-dev <56655493+stelly-dev@users.noreply.github.com>
2025-05-04 18:28:12 -0600

    added 404 images and some easter eggs

--- next.config.js
+++ next.config.js
@@ -2,7 +2,10 @@
 const nextConfig = {
   images: {
     remotePatterns: [new URL("https://picsum.photos/**")],
+    unoptimized: true,
   },
+  distDir: "build",
+  output: "export",
 };
 
 module.exports = nextConfig;
